d0d2c4de29a4a6a4ec3e5d98d719a325673a8646
Ray Kudjie <rkudjie@gmail.com>
2020-04-09 12:00:30 +0000

    challenge1

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,59 @@
 <?php
 
+function infectionProjection(int $currentlyInfected, array $data)
+{
+	$timeToElape = normalizeDuration($data["timeToElapse"], $data["periodType"]);
+  	
+	return $currentlyInfected * (pow(2 * intdiv($timeToElape, 3)));
+}
+
+
+function normalizeDuration(int $timeToElapse, String $periodType)
+{
+	if ($periodType === 'weeks') {
+    	return $timeToElapse * 7;
+	} 
+	 if ($periodType === 'months') {
+		return $timeToElapse * 30;
+	} 
+	
+	return $timeToElapse ;
+	
+}
+
+function impactEstimator($data)
+{
+	$impact = $data["reportedCases"] * 10;
+	
+	$infectionsByRequestedTime = infectionProjection($impact, $data );
+	
+	return [
+		  "currentlyInfected" => $impact,
+		  "infectionByRequestedTime" => $infectionsByRequestedTime
+	   ];
+}
+
+function severeImpactEstimator($data)
+{
+	$impact = $data["reportedCases"] * 50;
+
+	$infectionsByRequestedTime = infectionProjection( $impact, $data);
+	
+	return [
+		  "currentlyInfected" => $impact,
+		  "infectionByRequestedTime" => $infectionsByRequestedTime
+	  ];
+}
+
+
+
+ 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+
+  return json_encode([
+	  "data" => $data,
+	  "impact" => impactEstimator($data),
+	  "severeImpact" => severeImpactEstimator($data)
+  ]);
 }
